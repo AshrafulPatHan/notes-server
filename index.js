a9de2d901bf17baa7b76caaaba7975ce4afc0c48
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-17 17:56:59 +0600

    add database collection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -37,6 +37,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+// Data base collection
+const Database = client.db('Notes')
+const User = Database.collection('User')
+
+
+// End server
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
